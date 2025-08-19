7227cfd819638b3bb05c100b33018e3925132143
TaeHeon Ryoo <taroni55@naver.com>
2025-08-19 21:34:46 +0900

    [Create] 테스트 씬 구성

--- Assets/Scripts/YTH/Monster/MonsterModel.cs
+++ Assets/Scripts/YTH/Monster/MonsterModel.cs
@@ -1,18 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class MonsterModel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int _maxHp;
+    public int MaxHp { get => _maxHp; set => _maxHp = value; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] int _curHp;
+    public int CurHp { get => _curHp; set => _curHp = value; }
+
+    [SerializeField] int _moveSpeed;
+    public int MoveSpeed { get => _moveSpeed; set => _moveSpeed = value; }
 }
