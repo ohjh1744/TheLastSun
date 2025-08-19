333743cdc6d7e1c6a9a53a76406f09682cbf4e3f
TaeHeon Ryoo <taroni55@naver.com>
2025-08-19 21:36:09 +0900

    [Feat] 몬스터 경로 따라 이동 구현

--- Assets/Scripts/YTH/Wave/MonsterMover.cs
+++ Assets/Scripts/YTH/Wave/MonsterMover.cs
@@ -1,35 +1,41 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class MonsterMover : MonoBehaviour
 {
-    public List<Transform> pathPoints;
-    public float moveSpeed = 3f;
-    private int currentPointIndex = 0;
+    [SerializeField] List<Transform> _pathPoints;
 
-    void Start()
+    private int _currentPointIndex = 0;
+
+    private MonsterModel _model;
+
+    private void Awake()
     {
-        if (pathPoints.Count > 0)
-        {
-            StartCoroutine(MoveToNextPoint());
-        }
+        _model = GetComponent<MonsterModel>();
     }
 
+    private void Start()
+    {
+        StartCoroutine(MoveToNextPoint());
+    }
+
+    /// <summary>
+    /// 정해진 경로를 따라 몬스터가 움직임
+    /// </summary>
     IEnumerator MoveToNextPoint()
     {
-        while (currentPointIndex < pathPoints.Count)
+        while (_currentPointIndex < _pathPoints.Count)
         {
-            Transform targetPoint = pathPoints[currentPointIndex];
+            Transform targetPoint = _pathPoints[_currentPointIndex + 1];
             while (Vector3.Distance(transform.position, targetPoint.position) > 0.1f)
             {
-                transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, _model.MoveSpeed * Time.deltaTime);
                 yield return null;
             }
-            currentPointIndex++;
+            _currentPointIndex++;
         }
 
-        // 경로 끝에 도달했을 때 처리 (예: 적 파괴)
         Destroy(gameObject);
     }
 }
