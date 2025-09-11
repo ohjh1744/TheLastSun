6e9f1ee7276621ce6af6ee969fc84365c5edaadb
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2025-09-11 22:00:51 +0900

    [Feat] BossBookpanel 기능 구현
    에셋 정리 완료후 에셋적용하면 구현 마무리 완료 가능

---
+++ Assets/Scripts/OJH/Data/BossInfoData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "BossInfoData")]
+public class BossInfoData : ScriptableObject
+{
+    [SerializeField] private string _bossName;
+    public string BossName { get { return _bossName; } private set { } }
+
+    [SerializeField] private string _bossShortInfo;
+    public string BossShortInfo {  get { return _bossShortInfo;} private set { } }
+
+    [SerializeField] private string _bossLongInfo;
+    public string BossLongInfo {  get { return _bossLongInfo; } private set { } }
+}
