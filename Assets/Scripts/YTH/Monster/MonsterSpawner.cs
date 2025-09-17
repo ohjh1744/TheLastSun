1cd77e5577f3137ceed918dd4e09fea0e3c91f12
TaeHeon Ryoo <taroni55@naver.com>
2025-09-17 16:39:50 +0900

    [Feat] 몬스터 스포너 구현

---
+++ Assets/Scripts/YTH/Monster/MonsterSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    [SerializeField] int _spawnCount;
+
+    [SerializeField] Transform _spawnPoint;
+
+
+    private ObjectPool _objectPool => GetComponent<ObjectPool>();
+
+    private void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private void Spawn()
+    {
+        PooledObject pooledObject = _objectPool.GetPool(_spawnPoint.position, Quaternion.identity);
+    }
+
+    WaitForSeconds delay = new(2f);
+    IEnumerator SpawnRoutine()
+    {
+        for (int i = 0; i < _spawnCount; i++)
+        {
+            Spawn();
+            yield return delay;
+        }
+    }
+
+}
--- Assets/Scripts/YTH/Monster/PooledObject.cs
+++ Assets/Scripts/YTH/Monster/PooledObject.cs
@@ -5,22 +5,8 @@ using UnityEngine;
 public class PooledObject : MonoBehaviour
 {
     public ObjectPool returnPool;
-    [SerializeField] float returnTime;
+
     private float timer;
-
-    private void OnEnable()
-    {
-        timer = returnTime;
-    }
-
-    private void Update()
-    {
-        timer -= Time.deltaTime;
-        if (timer <= 0)
-        {
-            ReturnPool();
-        }
-    }
 
     public void ReturnPool()
     {
