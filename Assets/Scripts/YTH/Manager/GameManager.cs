5be50d610f3f8c4d29b2c9507593d4b7bdc04898
TaeHeon Ryoo <taroni55@naver.com>
2025-09-18 18:00:07 +0900

    [Feat] 배속 기능 구현

--- Assets/Scripts/YTH/Manager/GameManager.cs
+++ Assets/Scripts/YTH/Manager/GameManager.cs
@@ -1,10 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    private PlayerData _playerData;
 
+    private float _clearTime;
+
+    private void Awake()
+    {
+        SetSingleton();
+    }
+
+    #region ΩÃ±€≈Ê ºº∆√
+    public void SetSingleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    private void Update()
+    {
+        _clearTime += Time.deltaTime;
+    }
+
+    public void SetGameSpeed(float value)
+    {
+        Time.timeScale = value;
+    }
+
+    public void ClearStage()
+    {
+        _playerData.IsClearStage[_playerData.CurrentStage] = true;
+    }
+
+    public void RecordClearTime()
+    {
+        _playerData.ClearTimes[_playerData.CurrentStage] = _clearTime;
+    }
 }
