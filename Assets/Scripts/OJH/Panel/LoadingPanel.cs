b15ae5f61c7b8fc68f7fe211311797913c4cec3a
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2025-09-20 19:54:45 +0900

    [Feat] CreditPanel 구현
    1. creditPanel 구현
    2. LoadingPanel IAssetLodable 배열에서 List로 수정 및 CreditPanel 추가
    3. Settting Panel 배경이미지 어드레서블 Sprite 추가

--- Assets/Scripts/OJH/Panel/LoadingPanel.cs
+++ Assets/Scripts/OJH/Panel/LoadingPanel.cs
@@ -8,15 +8,13 @@ using UnityEditor.Hardware;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum IAssetLoad {Main, BossBook, Setting }
+public enum IAssetLoad {Main, BossBook, Setting, Credit }
 public class LoadingPanel : UIBInder
 {
     //어드레서블을 통해 불러와 적용할 에셋 개수
-    [SerializeField] private IAssetLoadable[] _assetLoadablePanels;
+    private List<IAssetLoadable> _assetLoadablePanels;
 
-    [SerializeField] private GameObject _mainPanel;
-    [SerializeField] private GameObject _boosBookPanel;
-    [SerializeField] private GameObject _settingPanel;
+    [SerializeField] private GameObject[] _panels;
 
     private Coroutine _routine;
     [SerializeField] private float _fakeLoadingTime;
@@ -29,7 +27,19 @@ public class LoadingPanel : UIBInder
     }
     private void Start()
     {
+        Init();
         _routine = StartCoroutine(CheckLoadAsset());
+    }
+
+    private void Init()
+    {
+        _assetLoadablePanels = new List<IAssetLoadable>();
+
+        for(int i = 0; i < _panels.Length; i++)
+        {
+            IAssetLoadable panel = _panels[i].GetComponent<IAssetLoadable>();
+            _assetLoadablePanels.Add(panel);
+        }
     }
 
     IEnumerator CheckLoadAsset()
@@ -39,7 +49,7 @@ public class LoadingPanel : UIBInder
         {
             bool isAnyNotClearLoad = false;
 
-            for (int i = 0; i < _assetLoadablePanels.Length; i++)
+            for (int i = 0; i < _assetLoadablePanels.Count; i++)
             {
                 // Panel들을 차례대로 탐색해서 Load클리어가 다 끝났는지 체크, 아니라면 종료하고 다시 재탐색
                 if (_assetLoadablePanels[i].LoadAssetUICount > _assetLoadablePanels[i].ClearLoadAssetCount)
@@ -83,11 +93,10 @@ public class LoadingPanel : UIBInder
         }
 
         // 다 되었따면 MainPanel 제외하고 다 꺼주기
-        _boosBookPanel.SetActive(false);
-        _settingPanel.SetActive(false);
+        _panels[(int)IAssetLoad.BossBook].SetActive(false);
+        _panels[(int)IAssetLoad.Setting].SetActive(false);
+        _panels[(int)IAssetLoad.Credit].SetActive(false);
         gameObject.SetActive(false);
-
-
     }
 
 }
