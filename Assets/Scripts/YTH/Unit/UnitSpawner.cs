32b35992fdab13c19813c38c3a2897c2e029e267
TaeHeon Ryoo <taroni55@naver.com>
2025-09-24 01:26:15 +0900

    [Feat] 유닛 스폰 기능 구현

--- Assets/Scripts/YTH/Unit/UnitSpawner.cs
+++ Assets/Scripts/YTH/Unit/UnitSpawner.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2[] _spawnPoint;
+
+    private int _curSpawnPoint;
+
+    private void Awake()
     {
-        
+        for (int i = 0; i < 4; i++)
+        {
+            _spawnPoint[i] = transform.GetChild(i).position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnUnit(GameObject unitPrefab)
     {
-        
+        Instantiate(unitPrefab, _spawnPoint[_curSpawnPoint], Quaternion.identity);
+
+        _curSpawnPoint = (_curSpawnPoint >= 3) ? 0 : _curSpawnPoint + 1;
     }
 }
