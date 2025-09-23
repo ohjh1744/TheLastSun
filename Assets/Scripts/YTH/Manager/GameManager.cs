e0b0e9b8e9e3f731fd81e3c7f1bb1199821dd1f8
TaeHeon Ryoo <taroni55@naver.com>
2025-09-24 00:32:14 +0900

    [Feat] 진행 시간 기록 기능 구현

--- Assets/Scripts/YTH/Manager/GameManager.cs
+++ Assets/Scripts/YTH/Manager/GameManager.cs
@@ -7,11 +7,13 @@ public class GameManager : MonoBehaviour
     private PlayerData _playerData;
 
     private float _clearTime;
+    private bool _isTimerRunning = false;
 
     private void Awake()
     {
         SetSingleton();
     }
+ 
 
     #region 싱글톤 세팅
     public void SetSingleton()
@@ -30,7 +32,25 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        _clearTime += Time.deltaTime;
+        // 타이머가 동작 중일 때만 시간 누적
+        if (_isTimerRunning)
+        {
+            _clearTime += Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// 게임 시작 시 타이머를 초기화하고 시작
+    /// </summary>
+    public void StartTimer()
+    {
+        _clearTime = 0f;
+        _isTimerRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        _isTimerRunning = false;
     }
 
     public void SetGameSpeed(float value)
@@ -41,6 +61,8 @@ public class GameManager : MonoBehaviour
     public void ClearStage()
     {
         _playerData.IsClearStage[_playerData.CurrentStage] = true;
+        StopTimer(); // 스테이지 클리어 시 타이머 멈춤
+        RecordClearTime(); // 클리어 시간 기록
     }
 
     public void RecordClearTime()
