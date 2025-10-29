7dc4c8deabe8c0beee1857e902eecc47e6397d80
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2025-10-29 14:54:32 +0900

    [Setting] 인게임쪽 Panel 로드 함수삭제

---
+++ Assets/Scripts/OJH/Context/InGame/InAddrContainer.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class InAddrContainer : UIBInder, IAssetLoadable
+{
+    #region IAssetLoadable 
+    //어드레서블을 통해 불러와 적용할 에셋 개수
+    [SerializeField] private int _loadAssetUICount;
+    public int LoadAssetUICount { get { return _loadAssetUICount; } set { _loadAssetUICount = value; } }
+
+    //현재 어드레서블을 통해 적용끝난 에셋 개수
+    private int _clearLoadAssetCount;
+    public int ClearLoadAssetCount { get { return _clearLoadAssetCount; } set { _clearLoadAssetCount = value; } }
+    #endregion
+
+    #region Addressable Assets
+    //UI
+    [SerializeField] AssetReferenceSprite _button_03;
+    [SerializeField] AssetReferenceSprite _button_04;
+    [SerializeField] AssetReferenceSprite _cancel;
+    [SerializeField] AssetReferenceSprite _diamond;
+    [SerializeField] AssetReferenceSprite _icon_Volume;
+    [SerializeField] AssetReferenceSprite _icon_VolumeMute;
+    [SerializeField] AssetReferenceSprite _pause;
+    [SerializeField] AssetReferenceSprite _popUp;
+    [SerializeField] AssetReferenceSprite _popUp_Title;
+    [SerializeField] AssetReferenceSprite _resourceBar1;
+    [SerializeField] AssetReferenceSprite _resourceBar2;
+    [SerializeField] List<AssetReferenceSprite> _backgroundSprites;
+    [SerializeField] List<AssetReferenceSprite> _tutorialSprites;
+    [SerializeField] List<AssetReferenceSprite> _wave1MobSprites;
+
+    //추후 넣을 것
+    [SerializeField] AssetReferenceSprite _spawnButton;
+    [SerializeField] AssetReferenceSprite _goSpecialSpawnButton;
+    [SerializeField] AssetReferenceSprite _upgradeWarriorButton;
+    [SerializeField] AssetReferenceSprite _upgradeArcherButton;
+    [SerializeField] AssetReferenceSprite _upgradeBomerButton;
+    [SerializeField] AssetReferenceSprite _specialSpawnButton;
+    [SerializeField] List<AssetReferenceSprite> _warriorPortraitSprites;
+    [SerializeField] List<AssetReferenceSprite> _archerPortraitSprites;
+    [SerializeField] List<AssetReferenceSprite> _bomerPortraitSprites;
+    [SerializeField] List<AssetReferenceSprite> _godPortraitSprites;
+
+
+    //Prefab
+    [SerializeField] List<AssetReferenceGameObject> _warriors;
+    [SerializeField] List<AssetReferenceGameObject> _archers;
+    [SerializeField] List<AssetReferenceGameObject> _bomers;
+    [SerializeField] List<AssetReferenceGameObject> _gods;
+
+    [SerializeField] List<AssetReferenceGameObject> _stage1Mobs;
+    [SerializeField] List<AssetReferenceGameObject> _stage2Mobs;
+    [SerializeField] List<AssetReferenceGameObject> _stage3Mobs;
+    [SerializeField] List<AssetReferenceGameObject> _stage4Mobs;
+    [SerializeField] List<AssetReferenceGameObject> _stage5Mobs;
+
+    [SerializeField] List<AssetReferenceGameObject> _heroPlate;
+    #endregion
+
+    void Awake()
+    {
+        BindAll();
+    }
+
+    void Start()
+    {
+        LoadAsset();
+    }
+
+    void LoadAsset()
+    {
+        AddressableManager.Instance.LoadOnlySprite(_button_03, (sprite) =>
+        {
+            _clearLoadAssetCount++;
+            GetUI<Image>("ClearPanelGoLobbyButton").sprite = sprite;
+            GetUI<Image>("TutorialButton").sprite = sprite;
+            GetUI<Image>("PausePanelGoLobbyButton").sprite = sprite;
+        });
+        AddressableManager.Instance.LoadOnlySprite(_button_04, (sprite) =>
+        {
+            _clearLoadAssetCount++;
+            GetUI<Image>("PauseBgImage").sprite = sprite;
+            GetUI<Image>("SoundBgImage").sprite = sprite;
+            GetUI<Image>("TImeSpeedButton").sprite = sprite;
+            GetUI<Image>("WaveInfoPanel").sprite = sprite;
+            GetUI<Image>("ShowJemBgImage").sprite = sprite;
+            GetUI<Image>("GoSellButton").sprite = sprite;
+            GetUI<Image>("ClearPanelSetFalseBgImage").sprite = sprite;
+            GetUI<Image>("PausePanelSetFalseBgImage").sprite = sprite;
+            GetUI<Image>("SpecialSpawnlSetFalseButtonBgImage").sprite = sprite;
+            GetUI<Image>("SellPanelSetFalseButtonBgImage").sprite = sprite;
+
+        });
+        AddressableManager.Instance.LoadOnlySprite(_cancel, (sprite) =>
+        {
+            _clearLoadAssetCount++;
+            GetUI<Image>("ShowJemImage").sprite = sprite;
+            GetUI<Image>("ClearPanelSetFalseButton").sprite = sprite;
+            GetUI<Image>("PausePanelSetFalseButton").sprite = sprite;
+            GetUI<Image>("SpecialSpawnSetFalseButton").sprite = sprite;
+            GetUI<Image>("SellPanelSetFalseButton").sprite = sprite;
+
+        });
+        AddressableManager.Instance.LoadOnlySprite(_diamond, (sprite) =>
+        {
+            _clearLoadAssetCount++;
+            GetUI<Image>("ShowJemImage").sprite = sprite;
+            GetUI<Image>("ShowWarriorUpgradeInfoJemImage").sprite = sprite;
+            GetUI<Image>("ShowArcherUpgradeInfoJemImage").sprite = sprite;
+            GetUI<Image>("ShowBomerUpgradeInfoJemImage").sprite = sprite;
+            GetUI<Image>("GetNormalJemImage").sprite = sprite;
+            GetUI<Image>("GetRareJemImage").sprite = sprite;
+            GetUI<Image>("GetAncientJemImage").sprite = sprite;
+            GetUI<Image>("GetLegendJemImage").sprite = sprite;
+            GetUI<Image>("GetEpicJemImage").sprite = sprite;
+        });
+        AddressableManager.Instance.LoadOnlySprite(_icon_Volume, (sprite) => { _clearLoadAssetCount++; GetUI<Image>("SoundButton").sprite = sprite; if (PlayerController.Instance.PlayerData.IsSound) { GetUI("SoundButton").SetActive(true); } });
+        AddressableManager.Instance.LoadOnlySprite(_icon_VolumeMute, (sprite) => { _clearLoadAssetCount++; GetUI<Image>("SoundMuteButton").sprite = sprite; if (!PlayerController.Instance.PlayerData.IsSound) { GetUI("SoundMuteButton").SetActive(true); } });
+        AddressableManager.Instance.LoadOnlySprite(_pause, (sprite) => { _clearLoadAssetCount++; GetUI<Image>("PauseButton").sprite = sprite; });
+        AddressableManager.Instance.LoadOnlySprite(_popUp, (sprite) =>
+        {
+            _clearLoadAssetCount++;
+            GetUI<Image>("BottomPanel").sprite = sprite;
+            GetUI<Image>("ClearPanel").sprite = sprite;
+            GetUI<Image>("PausePanel").sprite = sprite;
+            GetUI<Image>("SpecialSpawnPanel").sprite = sprite;
+            GetUI<Image>("SellPanel").sprite = sprite;
+        });
+        AddressableManager.Instance.LoadOnlySprite(_popUp_Title, (sprite) =>
+        {
+            _clearLoadAssetCount++;
+            GetUI<Image>("ClearPanelNameBgImage").sprite = sprite;
+            GetUI<Image>("PausePanelNameBgImage").sprite = sprite;
+            GetUI<Image>("SpecialSpawnNameBgImage").sprite = sprite;
+            GetUI<Image>("SellPanelNameBgImage").sprite = sprite;
+        });
+        AddressableManager.Instance.LoadOnlySprite(_resourceBar1, (sprite) => { _clearLoadAssetCount++;
+            GetUI<Image>("MobNumSliderBg").sprite = sprite;
+            GetUI<Image>("NotifyPanel").sprite = sprite;
+            GetUI<Image>("ChangeSellWarriorButton").sprite = sprite;
+            GetUI<Image>("ChangeSellArcherButton").sprite = sprite;
+            GetUI<Image>("ChangeSellBomerButton").sprite = sprite;
+        });
+        AddressableManager.Instance.LoadOnlySprite(_resourceBar2, (sprite) => { _clearLoadAssetCount++; GetUI<Image>("MobNumSliderFill").sprite = sprite; });
+        AddressableManager.Instance.LoadOnlySprite(_backgroundSprites[PlayerController.Instance.PlayerData.CurrentStage], (sprite) =>{_clearLoadAssetCount++;GetUI<Image>("InGameBgPanel").sprite= sprite;});
+        for (int i = 0; i < _tutorialSprites.Count; i++)
+        {
+            _clearLoadAssetCount++;
+            int index = i;
+            AddressableManager.Instance.LoadOnlySprite(_tutorialSprites[index], (sprite) => {
+                GetUI<Image>($"TutorialImage{index}").sprite = sprite;
+            });
+        }
+        AddressableManager.Instance.LoadOnlySprite(_wave1MobSprites[PlayerController.Instance.PlayerData.CurrentStage], (sprite) => { GetUI<Image>("WaveInfoMobImage").sprite = sprite; });
+
+    }
+
+
+#if UNITY_EDITOR
+    [ContextMenu("Fill StageMob")]
+    private void FillStageMob()
+    {
+        _stage5Mobs.Clear();
+
+        for (int i = 1; i <= 49; i++)
+        {
+            string assetPath = $"Assets/Prefabs/OJH/Monsters/Stage5/Stage5_Mob_{i}.prefab";
+
+            // Addressable에 이미 등록되어 있어야 합니다.
+            string addressableKey = AssetDatabase.AssetPathToGUID(assetPath); // GUID를 Key로 사용할 수 있음
+
+            AssetReferenceGameObject reference = new AssetReferenceGameObject(addressableKey);
+            _stage5Mobs.Add(reference);
+        }
+
+        EditorUtility.SetDirty(this);
+    }
+#endif
+}
--- Assets/Scripts/OJH/Panel/InGame/InGameMainPanel.cs
+++ Assets/Scripts/OJH/Panel/InGame/InGameMainPanel.cs
@@ -6,54 +6,6 @@ using UnityEngine.UI;
 
 public class InGameMainPanel : UIBInder, IAssetLoadable
 {
-    #region IAssetLoadable 
-    //어드레서블을 통해 불러와 적용할 에셋 개수
-    [SerializeField] private int _loadAssetUICount;
-    public int LoadAssetUICount { get { return _loadAssetUICount; } set { _loadAssetUICount = value; } }
-
-    //현재 어드레서블을 통해 적용끝난 에셋 개수
-    private int _clearLoadAssetCount;
-    public int ClearLoadAssetCount { get { return _clearLoadAssetCount; } set { _clearLoadAssetCount = value; } }
-    #endregion
-
-    #region Addressable Assets
-    //어드레서블
-    [Header("어드레서블 에셋")]
-    [SerializeField] private AssetReferenceSprite _buttonSprite;
-    [SerializeField] private AssetReferenceSprite _pauseSprite;
-    [SerializeField] private AssetReferenceSprite _soundOnSprite;
-    [SerializeField] private AssetReferenceSprite _soundOffSprite;
-    [SerializeField] private AssetReferenceSprite _popUpSprite;
-    [SerializeField] private AssetReferenceSprite _jemSprite;
-    [SerializeField] private AssetReferenceSprite _barBgSprite;
-    [SerializeField] private AssetReferenceSprite _barSprite;
-    [SerializeField] private AssetReferenceT<AudioClip>[] _bgmClips;
-    [SerializeField] private List<AssetReferenceSprite> _mob1Sprites;
-    [SerializeField] private List<AssetReferenceSprite> _mob2Sprites;
-    [SerializeField] private List<AssetReferenceSprite> _mob3Sprites;
-    [SerializeField] private List<AssetReferenceSprite> _mob4Sprites;
-    [SerializeField] private List<AssetReferenceSprite> _mob5Sprites;
-    [SerializeField] private AssetReferenceSprite[] _unitSprites;
-    #endregion
-
-    #region 저장해서 관리하는 에셋 혹은 UI
-    private Sprite _savedSoundOnSprite;
-    private Sprite _savedSoundOffSpirte;
-    private List<AssetReferenceSprite>[] _savedMobSprites;
-    private List<Sprite> _savedCurrentStageMobSprites;
-    private List<Color>[] _savedStageMobColors;
-    private List<Image> _upgradeButtonPortraitImages;
-
-    [Header("저장해서 사용하기 위한 변수")]
-    [SerializeField] private List<Color> _savedStage1MobColors;
-    [SerializeField] private List<Color> _savedStage2MobColors;
-    [SerializeField] private List<Color> _savedStage3MobColors;
-    [SerializeField] private List<Color> _savedStage4MobColors;
-    [SerializeField] private List<Color> _savedStage5MobColors;
-    #endregion
-
-    [SerializeField] private AudioSource _bgmAudio;
-
     private void Awake()
     {
         BindAll();
@@ -66,16 +18,7 @@ public class InGameMainPanel : UIBInder, IAssetLoadable
 
     private void Init()
     {
-        _savedMobSprites = new List<AssetReferenceSprite>[]{_mob1Sprites, _mob2Sprites, _mob3Sprites, _mob4Sprites, _mob5Sprites};
-
-        _savedCurrentStageMobSprites = new List<Sprite>();
-
-        _savedStageMobColors = new List<Color>[]{ _savedStage1MobColors, _savedStage2MobColors, _savedStage3MobColors, _savedStage4MobColors, _savedStage5MobColors};
-
-        _upgradeButtonPortraitImages = new List<Image> { GetUI<Image>("WarriorUpgradeButtonImage"), GetUI<Image>("ArcherUpgradeButtonImage"), GetUI<Image>("BomerUpgradeButtonImage") };
-
         AddEvent();
-        LoadAsset();
     }
 
     private void AddEvent()
@@ -84,127 +27,12 @@ public class InGameMainPanel : UIBInder, IAssetLoadable
     }
 
     //UI에 적용할 이미지들 불러오고 적용
-    private void LoadAsset()
-    {
-        AddressableManager.Instance.LoadOnlySprite(_buttonSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            GetUI<Image>("PauseBgImage").sprite = sprite;
-            GetUI<Image>("SoundBgImage").sprite = sprite;
-            GetUI<Image>("TImeSpeedButton").sprite = sprite;
-            GetUI<Image>("WaveInfoPanel").sprite = sprite;
-            GetUI<Image>("ShowJemBgImage").sprite = sprite;
-            GetUI<Image>("SpawnButtonBgImage").sprite = sprite;
-            GetUI<Image>("SpecialSpawnButtonBgImage").sprite = sprite;
-            GetUI<Image>("SellButtonBgImage").sprite = sprite;
-            GetUI<Image>("WarriorUpgradeButtonBgImage").sprite = sprite;
-            GetUI<Image>("ArcherUpgradeButtonBgImage").sprite = sprite;
-            GetUI<Image>("BomerUpgradeButtonBgImage").sprite = sprite;
-        });
-
-        AddressableManager.Instance.LoadOnlySprite(_pauseSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            GetUI<Image>("PauseButton").sprite = sprite;
-        });
-
-        AddressableManager.Instance.LoadOnlySprite(_soundOnSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            _savedSoundOnSprite = sprite;
-            SetorTurnSound(true);
-        });
-
-        AddressableManager.Instance.LoadOnlySprite(_soundOffSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            _savedSoundOffSpirte = sprite;
-            SetorTurnSound(true);
-        });
-
-        AddressableManager.Instance.LoadOnlySprite(_popUpSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            GetUI<Image>("BottomPanel").sprite = sprite;
-        });
-
-        AddressableManager.Instance.LoadOnlySprite(_jemSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            GetUI<Image>("ShowJemImage").sprite = sprite;
-            GetUI<Image>("SpawnButtonJemImage").sprite = sprite;
-            GetUI<Image>("SpecialShowJemButtonJemImage").sprite = sprite;
-            GetUI<Image>("ShowWarriorUpgradeInfoJemImage").sprite = sprite;
-            GetUI<Image>("ShowArcherUpgradeInfoJemImage").sprite = sprite;
-            GetUI<Image>("ShowBomerUpgradeInfoJemImage").sprite = sprite;
-        });
-
-        AddressableManager.Instance.LoadOnlySprite(_barBgSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            GetUI<Image>("MobNumSliderBg").sprite = sprite;
-        });
-
-        AddressableManager.Instance.LoadOnlySprite(_barSprite, (sprite) =>
-        {
-            _clearLoadAssetCount++;
-            GetUI<Image>("MobNumSliderFill").sprite = sprite;
-        });
-
-        int currentStage = PlayerController.Instance.PlayerData.CurrentStage;
-        AddressableManager.Instance.LoadSound(_bgmClips[currentStage], _bgmAudio, () => { _clearLoadAssetCount++; SetorTurnSound(true); });
-
-        for(int i = 0; i < _savedMobSprites[currentStage].Count; i++)
-        {
-            int index = i;
-            AddressableManager.Instance.LoadOnlySprite(_savedMobSprites[currentStage][index], (sprite) =>
-            {
-                _clearLoadAssetCount++;
-                _savedCurrentStageMobSprites.Add(sprite);
-                if (index == 0)
-                {
-                    GetUI<Image>("WaveInfoMobImage").sprite = _savedCurrentStageMobSprites[index];
-                    GetUI<Image>("WaveInfoMobImage").color = _savedStageMobColors[currentStage][index];
-                }
-            });
-        }
-
-        for(int i = 0; i < _unitSprites.Length; i++)
-        {
-            int index = i;
-            AddressableManager.Instance.LoadSprite(_unitSprites[index], _upgradeButtonPortraitImages[index], () =>
-            {
-                _clearLoadAssetCount++;
-            });
-        }
-
-    }
-
-    private void SetorTurnSound(bool isSet)
-    {
-        var playerData = PlayerController.Instance.PlayerData;
-        //변경하는 경우
-        if (isSet == false)
-        {
-            playerData.IsSound = !playerData.IsSound;
-        }
-        GetUI<Image>("SoundButton").sprite = playerData.IsSound ? _savedSoundOnSprite : _savedSoundOffSpirte;
-
-        //실제 사운드 조정
-        if (playerData.IsSound == true)
-        {
-            _bgmAudio.Play();
-        }
-        else if (playerData.IsSound == false)
-        {
-            _bgmAudio.Stop();
-        }
-    }
-
+  
+   
     [ContextMenu("FillSPrites")]
     private void FillStageMobSprites()
     {
-        _mob2Sprites.Clear();
+        //_mob2Sprites.Clear();
 
         for (int i = 1; i <= 49; i++)
         {
@@ -229,11 +57,11 @@ public class InGameMainPanel : UIBInder, IAssetLoadable
             string guid = AssetDatabase.AssetPathToGUID(spritePath);
 
             AssetReferenceSprite reference = new AssetReferenceSprite(guid);
-            _mob2Sprites.Add(reference);
+            //_mob2Sprites.Add(reference);
         }
 
         EditorUtility.SetDirty(this);
-        Debug.Log($"{_mob2Sprites.Count}개의 Stage2 Sprite 참조를 생성했습니다.");
+        //Debug.Log($"{_mob2Sprites.Count}개의 Stage2 Sprite 참조를 생성했습니다.");
     }
 
     [ContextMenu("Fill Colors")]
@@ -259,11 +87,11 @@ public class InGameMainPanel : UIBInder, IAssetLoadable
             }
 
             Color color = sr.color;
-            _savedStage5MobColors.Add(color);
+           // _savedStage5MobColors.Add(color);
         }
 
         EditorUtility.SetDirty(this);
-        Debug.Log($"{_savedStage5MobColors.Count}개의 색상을 성공적으로 추가했습니다!");
+       // Debug.Log($"{_savedStage5MobColors.Count}개의 색상을 성공적으로 추가했습니다!");
     }
 
 }
