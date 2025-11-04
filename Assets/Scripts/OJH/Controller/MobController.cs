201cceb155e19fe3c59fa7845f48c9f0afe06a41
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2025-11-04 20:34:09 +0900

    [Feat] 보스 Hp 표시를 위한 캔버스 Controller에 삽입

--- Assets/Scripts/OJH/Controller/MobController.cs
+++ Assets/Scripts/OJH/Controller/MobController.cs
@@ -1,16 +1,29 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+public enum EMobType {Normal, Boss }
 public class MobController : MonoBehaviour
 {
     [SerializeField] private MobData _mobData;
+
+    [SerializeField] EMobType _mobType;
+
+    [SerializeField] private Canvas _canvas;
 
     private int _curHp;
     public int CurHp { get { return _curHp; } set { _curHp = value; } }
 
 
+    private void Awake()
+    {
+        if(_mobType == EMobType.Boss)
+        {
+            _canvas.worldCamera = Camera.main;
+        }
+    }
 
     private void OnDisable()
     {
