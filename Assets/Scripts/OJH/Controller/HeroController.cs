cec05e47c921806711ae970718f177b58e5d9e47
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2025-11-05 18:01:31 +0900

    [Feat] 전설, 에픽, 신화 등장시 사운드 구현 완료

--- Assets/Scripts/OJH/Controller/HeroController.cs
+++ Assets/Scripts/OJH/Controller/HeroController.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class HeroController : MonoBehaviour
 {
     [SerializeField] private HeroData _heroData;
+
+    [SerializeField] private AudioSource _audio;
+
+    private void OnEnable()
+    {
+        SetSpawnSound();
+    }
+
+    private void SetSpawnSound()
+    {
+        if(PlayerController.Instance.PlayerData.IsSound == true)
+        {
+            if(_heroData.SpawnClip != null)
+            {
+                _audio.PlayOneShot(_heroData.SpawnClip);
+            }
+        }
+    }
 }
