bc0b1f91872f685f2d5c79f3cc86546519e94470
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2025-11-06 19:08:16 +0900

    [Feat] 튜토리얼 기능 구현 완료

--- Assets/Scripts/OJH/Panel/InGame/TutorialPanel.cs
+++ Assets/Scripts/OJH/Panel/InGame/TutorialPanel.cs
@@ -1,29 +1,108 @@
+using GooglePlayGames.BasicApi.SavedGame;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TutorialPanel : UIBInder
 {
 
+    [SerializeField] private int _tutorialMaxIndex;
+
+    private int _tutorialIndex;
+
+    WaitForSeconds _wsDelay;
+
     void Awake()
     {
         BindAll();
+        _wsDelay = new WaitForSeconds(0.1f);
     }
-    void Start()
+
+    private void OnEnable()
     {
         Init();
     }
 
+   
+    void Start()
+    {
+        DoAddEvent();
+    }
+
     private void Init()
     {
-        AddEvent();
+        //IsToturial false인 경우에만 해주기
+        if (PlayerController.Instance.PlayerData.IsTutorial == false)
+        {
+            SetIsTutorialTrue();
+        }
+
+        //튜토리얼 이미지 초기화
+        for (int i = 0; i < _tutorialMaxIndex; i++)
+        {
+            if (i == 0)
+            {
+                GetUI($"TutorialImage{i}").SetActive(true);
+            }
+            else
+            {
+                GetUI($"TutorialImage{i}").SetActive(false);
+            }
+        }
+    }
+
+    private void DoAddEvent()
+    {
+        for(int i = 0; i < _tutorialMaxIndex; i++)
+        {
+            AddEvent($"TutorialImage{i}", EventType.Click, DoNextTutorial);
+        }
+    }
+
+    private void DoNextTutorial(PointerEventData eventData)
+    {
+        _tutorialIndex++;
+
+        if(_tutorialIndex == _tutorialMaxIndex)
+        {
+            for (int i = 0; i < _tutorialMaxIndex; i++)
+            {
+                GetUI($"TutorialImage{i}").SetActive(false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _tutorialMaxIndex; i++)
+            {
+                if (_tutorialIndex == i)
+                {
+                    GetUI($"TutorialImage{i}").SetActive(true);
+                }
+                else if (_tutorialIndex != i)
+                {
+                    GetUI($"TutorialImage{i}").SetActive(false);
+                }
+            }
+        }
 
     }
-    private void AddEvent()
+
+    private void SetIsTutorialTrue()
     {
-       
+        StartCoroutine(OnSetIsTutorialTrue());
+    }
+
+    IEnumerator OnSetIsTutorialTrue()
+    {
+        while(NetworkCheckManager.Instance.IsConnected == false)
+        {
+            yield return _wsDelay;
+        }
+
+        GpgsManager.Instance.SaveData((success) =>{});
     }
 
 
