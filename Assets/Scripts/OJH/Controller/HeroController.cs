2f5cd63d43dc8b9d7d3c003d2915084b01a83da4
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2025-11-10 17:41:30 +0900

    [Feat] 유닛 배치 구현 완료

--- Assets/Scripts/OJH/Controller/HeroController.cs
+++ Assets/Scripts/OJH/Controller/HeroController.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class HeroController : MonoBehaviour
+public class HeroController : MonoBehaviour,IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
 
     [SerializeField] private HeroData _heroData;
@@ -16,6 +17,16 @@ public class HeroController : MonoBehaviour
 
     [SerializeField] Transform _attackPoint;
     public Transform AttackPoint { get { return _attackPoint; } set { _attackPoint = value; } }
+
+    private Vector2 _originalPosition;
+    private Camera _cam;
+    [SerializeField] private Vector2 _minBound;
+    [SerializeField] private Vector2 _maxBound;
+
+    private void Awake()
+    {
+        _cam = Camera.main;
+    }
 
     private void OnEnable()
     {
@@ -50,5 +61,29 @@ public class HeroController : MonoBehaviour
 
         // 원 그리기
         Gizmos.DrawWireSphere(center, _heroData.AttackRange);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("hhhi");
+        _originalPosition = transform.position;  // 누른 순간 위치 저장
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("hhhia");
+        // 마우스(터치)를 월드좌표로 변환
+        Vector2 worldPos = _cam.ScreenToWorldPoint(eventData.position);
+
+        // 사각형 영역 안으로 Clamp
+        worldPos.x = Mathf.Clamp(worldPos.x, _minBound.x, _maxBound.x);
+        worldPos.y = Mathf.Clamp(worldPos.y, _minBound.y, _maxBound.y);
+
+        transform.position = worldPos;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
     }
 }
